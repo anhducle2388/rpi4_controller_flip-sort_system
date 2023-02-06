62275b02efeb3f6f6589ab9360a7c6fa70a4ff3f
Duc Le <anhducle2388@gmail.com>
2023-02-07 01:03:23 +0700

    Update data_frame struct for IO module + Fix EcatDiag function for proper logging

--- src/app_threading.c
+++ src/app_threading.c
@@ -7,77 +7,34 @@
 
 extern int  OpMode, preOpMode;
 
-uint8_t stsUpdateVal = FALSE;
-uint8_t LED21 = 0x01;
-uint8_t LED43 = 0x84;
+extern cfgEcat cfgEcatJson;
 
-extern cfgEcat   cfgEcatJson; 
+do_KL2134t * do_KL2134_1, * do_KL2134_3;
+di_KL1104t * di_KL1104_1, * di_KL1104_3;
+ao_KL4002t * ao_KL4002;
+ai_KL3202t * ai_KL3202;
 
 // Function for Thread #1 IO Scanning Task
 void *Thread_IoTask(void *threadid) {
 
     char strMsg[250];
 
-    uint8_t i, j, oloop, iloop;
-    uint8_t preWkc = 0;
+    do_KL2134_1 = (do_KL2134t *) ec_slave[1].outputs + ec_slave[1].Obytes-2;
+    do_KL2134_3 = (do_KL2134t *) ec_slave[1].outputs + ec_slave[1].Obytes-1;
+
+    do_KL2134_1->data_frame = 0x01;
 
     retGoto_Thread1:
-        if (OpMode)
+    cfgEcatJson.curWkc = ec_receive_processdata(EC_TIMEOUTRET);
+    if (ec_slave[0].state == EC_STATE_OPERATIONAL) 
+    {
+        for (uint8_t i = 1; i <= ec_slavecount; i++)
         {
-            if ((ec_slave[0].state == EC_STATE_OPERATIONAL))
-            {
-                for (i = 1; i <= ec_slavecount; i++)
-                {
-                    oloop = ec_slave[i].Obytes;
-                    iloop = ec_slave[i].Ibytes;
 
-                    if (stsUpdateVal)
-                    {
-                        stsUpdateVal = FALSE;
-                        ec_slave[i].outputs[oloop - 1] = LED43;
-                        ec_slave[i].outputs[oloop - 2] = LED21;
-                    }
-
-                    ec_send_processdata();
-                    cfgEcatJson.curWkc = ec_receive_processdata(EC_TIMEOUTRET);
-                    
-                    // Log comm oper if the Wkc has been changed
-                    if(cfgEcatJson.curWkc != preWkc) {
-                        snprintf(strMsg, sizeof(strMsg), "[ECAT] Wkc has been changed from %d to %d", preWkc, cfgEcatJson.curWkc);
-                        logTsMsg(LOG_MSG, ECAT_SOEM_LPATH, strMsg);
-                    }
-
-                    // Log error if Wkc not at expected value.
-                    if(cfgEcatJson.curWkc < cfgEcatJson.expectedWkc) {
-                        snprintf(strMsg, sizeof(strMsg), "[ECAT] Wkc = %d < Expected Wkc %d", cfgEcatJson.curWkc, cfgEcatJson.expectedWkc);
-                        logTsMsg(ERR_MSG, ECAT_SOEM_LPATH, strMsg);                        
-                    }
-
-                    // if (( curWkc >= 3) && ((oloop > 0) || (iloop > 0)))
-                    // {
-                    //     char strTmp[100];
-                    //     snprintf(strMsg, sizeof(strMsg), "[THRD=1] [id=%d] [slv=%s] [wkc=%d] [O=]", i, ec_slave[i].name, curWkc);
-                    //     for(j = 0; j < oloop; j++)
-                    //     {
-                    //         snprintf(strTmp, sizeof(strTmp), "%3.2x", ec_slave[i].outputs[j]);
-                    //         strcat(strMsg, strTmp);
-                    //     }
-
-                    //     strcat(strMsg, " [I=]");
-                    //     for(j = 0; j < iloop; j++)
-                    //     {
-                    //         snprintf(strTmp, sizeof(strTmp), "%3.2x", ec_slave[i].inputs[j]);
-                    //         strcat(strMsg, strTmp);
-                    //     }
-                    //     logTsMsg(DBG_MSG, ECAT_SOEM_LPATH, strMsg);
-                    //     usleep(IO_TASK_INTERVAL_uS);
-                    // }
-                }
-            }
-            preWkc = cfgEcatJson.curWkc;
-            stsUpdateVal = 1;
         }
-        usleep(IO_TASK_INTERVAL_uS);
+    }
+    ec_send_processdata();
+    usleep(IO_TASK_INTERVAL_uS);
     goto retGoto_Thread1;
 }
 
@@ -91,20 +48,17 @@ void *Thread_DiagComm(void *threadid) {
 
 // Function for Thread #3
 void *Thread_Handler(void *threadid) {
+
     retGoto_Thread3:
+    // Tasking
+    // getEcatIoFrame(&cfgEcatJson);
+    do_KL2134_1->data_frame = ( do_KL2134_1->data_frame >> 1) | ( do_KL2134_1->data_frame << (4 - 1));
 
-        // Toggle LED
-        LED21 = LED21? 0: 0xf;
-
-        // Update new value
-        stsUpdateVal = TRUE;
-
-        // Sleep to wait
-        usleep(500000);
+    // Sleep to wait
+    usleep(1000000);
 
     goto retGoto_Thread3;
 }
-
 
 // Function to initialize and init thread control
 int cfgThreadMap() {
